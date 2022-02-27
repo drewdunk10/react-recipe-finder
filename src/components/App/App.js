df5f4e2f3b55bb5803b215d762dca90248e87298
Drew Dunkelberger <drewdunk5841@gmail.com>
2022-02-27 14:42:32 -0500

    Add basic search functionality.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,46 @@
-import logo from '../../logo.svg';
 import UserContext from "../User/User";
 import Search from "../Search/Search";
 import RecipeCard from "../RecipeCard/RecipeCard"
 import './App.css';
+import {useState} from "react";
 
 const user = {
   name: 'Drew'
 }
 
 function App() {
-  return (
+    const [recipes, setRecipes] = useState([])
+
+    return (
       // Give all child components access to user context.
       <UserContext.Provider value={user}>
         <div className="App">
           <header className="App-header">
-            {/* TODO: Update recipe cards when search button is clicked */}
-            <RecipeCard recipe={Search.fetchRecipes("steak")}/>
-            <button type={"button"}>Submit</button>
             <p>
               Edit <code>src/App.js</code> and save to reload.
             </p>
           </header>
+            <form>
+                <label>
+                    Name:
+                    <input type="text" name="name" />
+                </label>
+                <button type={"button"} onClick={async () => setRecipes(await Search.fetchRecipes(document.querySelector("input").value))}>Submit</button>
+                {
+                    recipes.map(recipe =>
+                        <RecipeCard
+                            key={recipe.name}
+                            image={recipe.image}
+                            name={recipe.name}
+                            cookTime={recipe.cookTime}
+                            prepTime={recipe.prepTime}
+                            recipeYield={recipe.recipeYield}
+                            desc={recipe.description}
+                            ingredients={recipe.ingredients}
+                        />
+                    )
+                }
+            </form>
         </div>
       </UserContext.Provider>
   );
--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -1,19 +1,19 @@
 export default RecipeCard;
 
-function RecipeCard({recipe}) {
-    console.log(recipe)
+// TODO: Create a separate view/page for viewing ingredients and description
+function RecipeCard({image, name, cookTime, prepTime, recipeYield, desc, ingredients}) {
     return(
         <article className="recipe-card">;
-            <img className="recipe" src={recipe[0].image} alt="Recipe for ${recipe.name}."/>;
+            <img className="recipe" src={image} alt="Recipe for ${recipe.name}."/>;
                 <section className="event-card-body">
-                    <h3>{recipe.name}</h3>
-                    <p>{recipe.cookTime}</p>
-                    <p>{recipe.prepTime}</p>
-                    <p>{recipe.recipeYield}</p>
+                    <h3>{name}</h3>
+                    <p><strong>Cook Time: </strong>{cookTime}</p>
+                    <p><strong>Prep Time: </strong>{prepTime}</p>
+                    <p><strong>Yield: </strong>{recipeYield}</p>
                     <button type="button" onClick={onView}>View Recipe</button>
                     <section className="hidden-desc">
-                        <p>{recipe.description}</p>
-                        <p>{recipe.ingredients}</p>
+                        <p>{desc}</p>
+                        <p>{ingredients}</p>
                     </section>
                 </section>
         </article>
